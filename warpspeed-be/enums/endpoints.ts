0dce4aaf8620d6d6c9234c1c35e8e4b1609fadf7
Jayanth <ae20b030@smail.iitm.ac.in>
2023-05-14 11:32:45 +0530

    Updated endpoints

--- warpspeed-be/enums/endpoints.ts
+++ warpspeed-be/enums/endpoints.ts
@@ -6,7 +6,7 @@ const apiUrl = process.env.API_URL as string
 
 export const endpoints = {
     CHATCOMPLETION: "https://api.openai.com/v1/chat/completions",
-    GEN_ARCH: apiUrl + "/generateArchitecture",
-    GEN_SYS_DES: apiUrl + "/generateSysDes",
-    GEN_IAC: apiUrl + "/generateIaC",
+    GEN_ARCH: apiUrl + "/get-cloud-architecture",
+    GEN_SYS_DES: apiUrl + "/get-system-diagram",
+    GEN_IAC: apiUrl + "/get-cloud-iac",
 } as const
