0bf92240129cc6ed687be31b023fbf643113c029
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-05-16 18:56:48 +0530

    remove this

--- warpspeed-be/utils/getGenerateArchitecturePrompt.ts
+++ warpspeed-be/utils/getGenerateArchitecturePrompt.ts
@@ -1,5 +1,5 @@
-export const getGenerateArchitecturePrompt = (input:string)=>{
+export const getGenerateArchitecturePrompt = (input: string) => {
     return `
-    Extract a rough overview and key technical 5 points which are not more than 20 words long from this data: ${input}.
+    Extract a rough overview and key technical 5 points which are not more than 20 words long from this data containing the overview and some important question answers about the requirement: ${input}.
     `
 }
